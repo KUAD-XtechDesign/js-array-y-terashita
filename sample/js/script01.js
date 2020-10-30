0d4bab6decb6a8c31797eed191e6ae5ebcbe66f8
Y- terashita <y-terashita4278@st.kyoto-art.ac.jp>
2020-10-30 16:08:58 +0900

    練習問題

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -6,7 +6,36 @@ $(function(){
     console.log(students)
   })
 
- 
+  $("#button01").on("click",function(){
+    /*console.log("クリックされた")*/
+    /*console.log(students[0],students.length);*/
+    $("#content").text("最初のメンバーは" + students[0] + "/人数は" + students.length + "人");
+  })
 
+  $("#button02").on("click",function(){
+    $("#content").text(students[2]);
+  })
+ 
+  $("#button03").on("click",function(){
+    /*$("content").text(students[4])*/
+    $("#content").text(students[students.length-1]);
+  })
+
+  $("#button04").on("click",function(){
+    /*students[5] = "山下"*/
+    /*students[students.length] = "山下"*/
+    /* "練習問題6-4" students.push("山下")*/
+    if(students[students.length-1]!="山下"){
+      students.push("山下")
+    }
+  })
+
+  $("#button05").on("click",function(){
+    students = students.reverse()
+  })
+
+  /*$("#button06").on("click",function(){
+    students[];
+  })*/
 
 })
--- sample/js/script02.js
+++ sample/js/script02.js
@@ -5,10 +5,29 @@ $(function(){
   $("#button00").on("click",function(){
     console.log(students)
   })
-  
 
+  /*let txt = ""*/
 
+  $("#button01").on("click",function(){
+    let txt = ""
+    for(let i=0; i<students.length; i++){
+      txt +=students[i]
+      /*$("#content").text(students[i]);*/
+      /*$("#content").append(students[i]);*/
+    }
+    $("#content").text(txt);
 
+  })
 
+  $("#button02").on("click",function(){
+    let txt = ""
+    for(let i=0; i<students.length; i++){
+      txt +="<div class='student_name' id='student"+i+"'>" + students[i] + "</div>"
+      /*txt +="<div>" + students[i] + "</div>"*/
+    }
+
+    $("#content").html(txt);
+
+  })
 
 })
